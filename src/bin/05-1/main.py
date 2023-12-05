a664aa8a304424f70b3fed51bd2cae39990ec016
Maxence Groine <maxence@groine.fr>
2023-12-05 16:16:46 +0100

    05-1: Completed

--- src/bin/05-1/main.py
+++ src/bin/05-1/main.py
@@ -1,13 +1,9 @@
-import sys
-
 with open('./data/05-1/input', 'r') as input_file:
-    input = input_file.readlines()
+    inp = input_file.readlines()
 
-seeds = [(0, int(i.strip())) for i in input[0].split(': ')[1].split(' ')]
+seeds = [(0, int(i.strip())) for i in inp[0].split(': ')[1].split(' ')]
 table = 1
-print(seeds)
-print(f"Switching to table \"seed-to-soil\"")
-for line_idx, line in enumerate(input[3:]):
+for line_idx, line in enumerate(inp[3:]):
     line = line.strip()
     if len(line) == 0:
         continue
@@ -16,9 +12,7 @@ for line_idx, line in enumerate(input[3:]):
         for seed_idx, seed_struct in enumerate(seeds):
             seed_table, seed = seed_struct[0], seed_struct[1]
             if seed_table != table:
-                print(f"Seed {seed} had no mapping in table. Keeping the same seed.")
                 seeds[seed_idx] = (table, seed)
-        print(f"Switching to table \"{line[:-5]}\"")
         table += 1
         continue
 
@@ -26,14 +20,12 @@ for line_idx, line in enumerate(input[3:]):
     for seed_idx, seed_struct in enumerate(seeds):
         seed_table, seed = seed_struct[0], seed_struct[1]
 
-        if seed_table != table:
+        if seed_table == table:
             continue
 
         if seed >= src and seed <= src + src_range - 1:
             diff = seed - src
-            print(f"Seed {seed} (index {seed_idx}) has a match and is replaced by value {dest+diff}")
-            seeds[seed_idx] = (seed_table + 1, dest+diff)
+            seeds[seed_idx] = (table, dest+diff)
 
-print(seeds)
 the_min = min([i[1] for i in seeds])
-print(f"{the_min} / idx = {[i[1] for i in seeds].index(the_min)}")
+print(f"{the_min}")
